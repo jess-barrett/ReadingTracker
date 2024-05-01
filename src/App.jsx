2e15380caca2fad2ce77f26d1b00183e02741f8e
jess-barrett <jdbarrett@ksu.edu>
2024-05-01 00:08:18 -0500

    Prettier formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
