1a4d8e262b4cf6c076cb73956d3549bfd38898f3
jess-barrett <jdbarrett@ksu.edu>
2024-05-03 17:45:53 -0500

    My Shelf button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import bookshelf from "../assets/images/bookshelf.jpg";
-import library3 from "../assets/images/library3.png";
+import library from "../assets/images/library3.png";
 
 const Hero = () => {
   return (
@@ -8,8 +7,17 @@ const Hero = () => {
       <section class="h-screen flex bg-white">
         <div class="w-full m-4">
           <div class="rounded-md items-center">
-            <div className="h-3/4">
-              <img className="rounded-md w-full" src={library3} alt="Bookshelf"/>
+            <div className="grid grid-cols-12 grid-rows-8">
+              <img
+                className="rounded-md w-full col-span-full row-span-full mb-4"
+                src={library}
+                alt="Library"
+              />
+              <div className="flex items-center justify-center mr-12 mt-6 col-start-9 col-span-2 row-start-1 row-end-5 opacity-0 hover:opacity-100">
+                <button className="bg-orange-500 hover:bg-orange-400 text-white text-2xl font-bold text-center px-4 py-2 rounded-md">
+                  My Shelf
+                </button>
+              </div>
             </div>
           </div>
         </div>
